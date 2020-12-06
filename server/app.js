7364dcee4765d1889ddbc8d4af49586f12bc50da
Steven Cho <sxc6061@g.rit.edu>
2020-12-06 14:50:59 -0800

    fixes

--- server/app.js
+++ server/app.js
@@ -14,76 +14,76 @@ const csrf = require('csurf');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/PokemonGatcha';
+const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/igme430-project-2';
 
-const moongooseOptions = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+const mongooseOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 };
-
-mongoose.connect(dbURL, moongooseOptions, (err) => {
-    if (err){
-        console.log('Could not connect to database');
-        throw err;
-    }
+mongoose.connect(dbURL, mongooseOptions, (err) => {
+  if (err) {
+    console.log('Could not connect to database');
+    throw err;
+  }
 });
-
 let redisURL = {
-    hostname: 'redis-13818.c241.us-east-1-4.ec2.cloud.redislabs.com',
-    port: 13818,
+  hostname: 'redis-13818.c241.us-east-1-4.ec2.cloud.redislabs.com',
+  port: 19669,
 };
-
 let redisPASS = 'ZX5HroH35NRU7V0sdI0eR7Zs28vfZt82';
 if (process.env.REDISCLOUD_URL) {
-    redisURL = url.parse(process.env.REDISCLOUD_URL);
-    [, redisPASS] = redisURL.auth.split(':');
+  redisURL = url.parse(process.env.REDISCLOUD_URL);
+  [, redisPASS] = redisURL.auth.split(':');
 }
-let redisClient = redis.createClient({
-    host: redisURL.hostname,
-    port: redisURL.port,
-    password: redisPASS,
+const redisClient = redis.createClient({
+  host: redisURL.hostname,
+  port: redisURL.port,
+  password: redisPASS,
 });
 
 const router = require('./router.js');
 
 const app = express();
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
-app.use(favicon(`${__dirname}/../hosted/img/pokeball.png`));
 app.disable('x-powered-by');
+app.use(cookieParser());
+
+app.use(favicon(`${__dirname}/../hosted/assets/favicon.png`));
 app.use(compression());
 app.use(bodyParser.urlencoded({
-    extended: true,
+  extended: true,
 }));
 app.use(session({
-    key: 'sessionid',
-    store: new RedisStore({
-        client: redisClient,
-    }),
-    secret: 'Gotta Catch Em All',
-    resave: true,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-    },
+  key: 'sessionid',
+  store: new RedisStore({
+    client: redisClient,
+  }),
+  secret: 'Catch em all',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+  },
 }));
+
+
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
-app.use(cookieParser());
-
 app.use(csrf());
 app.use((err, req, res, next) => {
-    if (err.code !== 'EBADCSRFTOKEN') return next(err);
+  console.log(err.code);
+  if (err.code !== 'EBADCSRFTOKEN') return next(err);
 
-    console.log('Missing CSRF token');
-    return false;
+  console.log('Missing CSRF token');
+  return false;
 });
 
 router(app);
 
 app.listen(port, (err) => {
-    if (err){
-        throw err;
-    }
-    console.log(`Listening on port ${port}`);
+  if (err) {
+    throw err;
+  }
+  console.log(`Listening on port ${port}`);
 });
